5397de642c1d89ea5534ee6f9af9af605fe887e0
AnishBaniya1 <112795330+AnishBaniya1@users.noreply.github.com>
2024-06-05 22:39:00 +0545

    Add files via upload

---
+++ day7-calc.py
@@ -0,0 +1,10 @@
+a= int (input("Enter first Number:"))
+b= int (input("Enter second Number:"))
+add=a+b
+print("The addition is:",add)
+sub=a-b
+print("The subtraction is:",sub)
+mul=a*b
+print("The multiplication is:",mul)
+div=a/b
+print("The division is:",div)
